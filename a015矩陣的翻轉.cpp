c306fd69fe4625b7d8e9c33b3e860850e30b6198
s1130128-hash <s1130128@dmhs.kh.edu.tw>
2026-04-28 11:01:22 +0800

    Create a015矩陣的翻轉.cpp

---
+++ a015矩陣的翻轉.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int r, c;
+	unt a[100][100];
+	while (cin >> r >> c) {
+		for (int i=0; i<r ; i++) {
+			for (int j=0; j<c; j++) {
+				cin >> a[i][j];
+			}
+		}
+		for (int i=0; i<c; i++) 
+			for (int j=0; j<r; J++) {
+				cout << a[j][i] << " ";
+			}
+		cout << "\n";
+	}
+	return 0;
+}
